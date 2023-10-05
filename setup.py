fe2b9018c3fc91fea13075768d5d8082046581c6
Seanghay Yath <seanghay.dev@gmail.com>
2023-10-05 22:31:01 +0700

    use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,34 +1,10 @@
-import setuptools
-from Cython.Build import cythonize
+from setuptools import Extension, setup
 
-with open("README.md", "r") as f:
-    long_description = f.read()
-
-setuptools.setup(
-    ext_modules = cythonize("khmerphonemizer/g2p_phonetisaurus.pyx"),
-    name="khmerphonemizer",
-    version="0.0.2",
-    description="A Free, Standalone and Open-Source Khmer Grapheme-to-Phonemes.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/seanghay/khmerphonemizer",
-    author="Seanghay Yath",
-    author_email="seanghay.dev@gmail.com",
-    license="MIT",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-    ],
-    python_requires=">=3.8",
-    packages=setuptools.find_packages(exclude=["examples"]),
-    package_dir={"khmerphonemizer": "khmerphonemizer"},
-    package_data={"khmerphonemizer": ["km_phonemizer.npz", "km_lexicon.tsv"]},
-    include_package_data=True,
-    install_requires=[
-        "cython",
-        "numpy>=1.19.0,<2.0.0",
-        "khmercut",
-    ],
+setup(
+    ext_modules=[
+        Extension(
+            name="khmerphonemizer.g2p_phonetisaurus",
+            sources=["khmerphonemizer/g2p_phonetisaurus.pyx"],
+        ),
+    ]
 )
