6e310089707439cb265e67961ebafcb934e67627
Seanghay Yath <seanghay.dev@gmail.com>
2023-12-19 16:38:39 +0700

    0.0.3

--- examples/single.py
+++ examples/single.py
@@ -3,3 +3,4 @@ from khmerphonemizer import phonemize_single
 if __name__ == "__main__":
   assert phonemize_single("មាន់") == ['m', 'ŏ', 'ə', 'n']
   assert phonemize_single("មិនទាន់ទៅទេ") == ['m', 'ɨ', 'n', 't', 'ŏ', 'ə', 'n', 't', 'ɨ', 'w', 't', 'eː']
+  
--- khmerphonemizer/__init__.py
+++ khmerphonemizer/__init__.py
@@ -3,9 +3,10 @@ Khmer Phonemizer - A Free, Standalone and Open-Source Khmer Grapheme-to-Phonemes
 """
 import os
 import csv
-import khmercut
 import re
-from khmerphonemizer.g2p_phonetisaurus import PhonetisaurusGraph
+from khmercut import tokenize
+from sosap import Model
+
 
 def _read_lexicon_file(file):
     lexicon = {}
@@ -18,27 +19,21 @@ def _read_lexicon_file(file):
 
 
 _pattern = re.compile(r"([A-Za-z\u1780-\u17ff]+)|([^A-Za-z\u1780-\u17ff]+)")
-_graph_file = os.path.join(os.path.dirname(__file__), "km_phonemizer.npz")
+_model_file = os.path.join(os.path.dirname(__file__), "g2p.fst")
 _lexicon_file = os.path.join(os.path.dirname(__file__), "km_lexicon.tsv")
 _lexicon_dict = _read_lexicon_file(_lexicon_file)
-_graph = PhonetisaurusGraph.load(_graph_file, preload=False)
+_model = Model(_model_file)
 
 
-def _phoneticize(word: str, beam: int, min_beam: int, beam_scale: float):
-    results = _graph.g2p_one(word, beam=beam, min_beam=min_beam, beam_scale=beam_scale)
+def _phoneticize(word: str):
+    results = _model.phoneticize(word)
     results = list(results)
     if len(results) == 0:
         return None
-    return results[0]
+    return results
 
 
-def phonemize_single(
-    word,
-    beam: int = 500,
-    min_beam: int = 100,
-    beam_scale: float = 0.6,
-    use_lexicon: bool = True,
-):
+def phonemize_single(word, use_lexicon: bool = True):
     r"""
     Phonemize a single word. The word must match [a-zA-Z\u1780-\u17dd]+
     """
@@ -47,21 +42,18 @@ def phonemize_single(
     word = word.lower()
     if use_lexicon and word in _lexicon_dict:
         return _lexicon_dict[word]
-    return _phoneticize(word, beam=beam, min_beam=min_beam, beam_scale=beam_scale)
+    return _phoneticize(word)
 
 
 def phonemize(
     input_str: str,
-    beam: int = 500,
-    min_beam: int = 100,
-    beam_scale: float = 0.6,
     use_lexicon: bool = True,
 ):
     r"""
     Tokenize and Phonemize text with multiple words into list of phonemes
     """
     outputs = []
-    tokens = khmercut.tokenize(input_str)
+    tokens = tokenize(input_str)
     for token in tokens:
         for m in _pattern.finditer(token):
             if m[2] is not None:
@@ -69,9 +61,6 @@ def phonemize(
                 continue
             phonemes = phonemize_single(
                 m[1].lower(),
-                beam=beam,
-                min_beam=min_beam,
-                beam_scale=beam_scale,
                 use_lexicon=use_lexicon,
             )
             outputs.append(phonemes)
